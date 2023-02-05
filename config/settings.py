206b3d1628858ef71390ae12e96a08c2606b5379
Pavle Cvejovic <pavle.cvejovic@qcerris.com>
2023-02-05 19:05:33 +0100

    minor fix

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
